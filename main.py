bed892f495b7d2430469709c1e1fc1e5bd44f9ca
Owen M. Peoples <peoplowe000@warren.k12.in.us>
2024-10-22 10:12:08 -0400

    2nd commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# TODO: Use the input function for card1
+# TODO: Use the input function for card2
+# Don't forget int
+card1 = int(input("What is your first card?:"))
+card2 = int(input("What is your second card?:"))
+
+
+card_sum = card1 + card2
+
+is_21_or_over = card_sum >= 21
+
+# TODO: print the result
+print(is_21_or_over)
